443d96b5abb04f6570a52ac760008874a65cca13
Pete Hamrick <pete.hamrick@gmail.com>
2021-09-27 13:58:47 -0700

    wrote get film failing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,22 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('film routes', () => {
     beforeEach(() => {
         return setup(pool);
+    });
+
+    it('should get the first star wars film and show the title, release date, episode id, and director', () => {
+        return request(app)
+            .get('/api/films/1')
+            .then((res) => {
+                expect(res.body).toEqual({
+                    title: expect.any(String),
+                    releaseDate: expect.any(String),
+                    episodeId: expect.any(String),
+                    director: expect.any(String),
+                });
+            });
     });
 
     afterAll(() => {
