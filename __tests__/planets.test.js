07f604d8e9a420823ecc6703eaf61e268519b4fe
Pete Hamrick <pete.hamrick@gmail.com>
2021-09-28 15:46:36 -0700

    wrote save random planet failing test

--- __tests__/planets.test.js
+++ __tests__/planets.test.js
@@ -2,10 +2,25 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import { get } from 'express/lib/response';
 
 describe('Planet routes', () => {
     beforeEach(() => {
         return setup(pool);
+    });
+
+    it('should get, store and return a random planet', () => {
+        return request(app)
+            .get('/api/planets/random')
+            .then((res) => {
+                expect(res.body).toEqual({
+                    id: 1,
+                    name: expect.any(String),
+                    climate: expect.any(String),
+                    terrain: expect.any(String),
+                    population: expect.any(String),
+                });
+            });
     });
 
     afterAll(() => {
